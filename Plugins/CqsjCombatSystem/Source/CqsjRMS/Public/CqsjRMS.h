86d8667b0a787d02ff4b64ffa456c20d3613549b
2445968904 <44425297+2445968904@users.noreply.github.com>
2023-12-06 14:14:25 +0800

    AddSomeRMSFile

---
+++ Plugins/CqsjCombatSystem/Source/CqsjRMS/Public/CqsjRMS.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "CqsjRMS.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CQSJRMS_API UCqsjRMS : public UObject
+{
+	GENERATED_BODY()
+};
